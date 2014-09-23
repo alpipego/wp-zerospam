88f46f8f7877af05de8f2b6785821de9631fb376
alpipego <alpipego@gmail.com>
2014-09-23 18:00:57 +0200

    initial plugin file

---
+++ wp-zerospam.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Zero Null Nada Spam
+Plugin URI: https://github.com/alpipego/wp-zerospam
+Description: A WordPress Plugin to get rid of comment spam as Akisment/Anti Spam Bee an alike all failed. Loosely based on a technique I have been using for years in contact forms and http://davidwalsh.name/wordpress-comment-spam
+Author: Alex
+Version: 0.1
+Author URI: http://alpipego.com/
+*/
