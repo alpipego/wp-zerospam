2fe4812d321d5776b3edf58e1f78ca09a3c37d56
alpipego <alpipego@gmail.com>
2014-09-23 18:35:32 +0200

    added tommcfarlins boilerplate

---
+++ public/views/public.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Represents the view for the public-facing component of the plugin.
+ *
+ * This typically includes any information, if any, that is rendered to the
+ * frontend of the theme when the plugin is activated.
+ *
+ * @package   Zero_Null_Nada_Spam
+ * @author    Alex <alpipego@gmail.com>
+ * @license   GPL-2.0+
+ * @link      http://alpipego.com/
+ * @copyright 2014 Alex
+ */
+?>
+
+<!-- This file is used to markup the public facing aspect of the plugin. -->
---
+++ uninstall.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Fired when the plugin is uninstalled.
+ *
+ * @package   Zero_Null_Nada_Spam
+ * @author    Alex <alpipego@gmail.com>
+ * @license   GPL-2.0+
+ * @link      http://alpipego.com/
+ * @copyright 2014 Alex
+ */
+
+// If uninstall not called from WordPress, then exit
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
+
+// @TODO: Define uninstall functionality here
