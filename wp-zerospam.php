efc56d0920c3e16230f1e4cfeac5b3ae9bce0aae
alpipego <alpipego@gmail.com>
2014-09-24 15:24:54 +0200

    get the enqueued stylesheets

--- wp-zerospam.php
+++ wp-zerospam.php
@@ -7,3 +7,39 @@ Author: Alex
 Version: 0.1
 Author URI: http://alpipego.com/
 */
+
+class WPZeroSpam 
+{
+
+    public function __contstruct() 
+    {
+        
+    }
+
+    /**
+     * get the stylesheet we're going to inject our styles into
+     * @return string $handle stylesheet handle
+     */
+     
+    private function _styles() 
+    {        
+        global $wp_styles;
+        $queue = array_reverse($wp_styles->queue);
+        foreach ($queue as $handle) {
+            if (! array_key_exists('conditional', $wp_styles->registered[$handle]->extra)) {
+                return $handle;
+            }
+        }
+    }
+
+}
+
+function nospam() {
+    $nospam = new WPZeroSpam();
+    global $wp_styles;
+    echo '<code><pre>';
+        // var_dump($wp_styles->registered['colors']);
+        var_dump($nospam->styles());
+    echo '</pre></code>';
+}
+add_action('wp_footer', 'nospam');
